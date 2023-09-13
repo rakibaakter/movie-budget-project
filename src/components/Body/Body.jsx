0412727e49e350893a801a4bea29fce08768ddd9
rakibaakter <rakibaakter635@gmail.com>
2023-09-14 02:00:14 +0600

    condition to hired on  budget

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -14,10 +14,27 @@ const Body = () => {
 
     // add to cart
     const [cartItems, setCartItems] = useState([]);
+    const totalBudget = 25000;
+    const [totalCost, setTotalCost] = useState(0);
+    const [remaining, setRemaining] = useState(totalBudget);
+    // console.log(totalBudget, remaining)
     const cartHandler = (actor) =>{
-        console.log(actor);
-        console.log(cartItems);
-        setCartItems([...cartItems, actor]);
+        // console.log(actor);
+        // console.log(cartItems);
+        const isAdded = cartItems.find(added=>added.id === actor.id);
+        if(isAdded){
+            return alert(`${actor.name} is already hired`);
+        }
+        if(remaining > actor.salary){
+            const newtotalCost = totalCost + actor.salary;
+            const newRemaining = totalBudget - newtotalCost;
+            setTotalCost(newtotalCost)
+            setRemaining(newRemaining)
+            setCartItems([...cartItems, actor]);
+        }else{
+            return alert("You can't hire")
+        }
+        
     }
     return (
         <div className='body-container'>
@@ -31,7 +48,13 @@ const Body = () => {
                 )
             }
             </div>
-            <Cart cartItems={cartItems}></Cart>
+            <Cart 
+                cartItems={cartItems}
+                totalBudget={totalBudget}
+                remaining={remaining}
+                totalCost={totalCost}
+            >
+            </Cart>
         </div>
     );
 };
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,21 @@
 import './Cart.css'
 
-const Cart = ({cartItems}) => {
+const Cart = ({cartItems, totalBudget, remaining, totalCost}) => {
     // console.log(cartItems)
     // const {name} = cartItems;
     return (
         <div className='cart-container'>
-            <h2>Add to Cart {cartItems.length}</h2>
-            <ul>
+            <h2>Hired List {cartItems.length}</h2>
+            <h3>Total Budget : ${totalBudget}</h3>
+            <p>Total Cost : ${totalCost}</p>
+            <p>Remaining Budget : ${remaining}</p>
+            <ul className='list-container'>
             {
                 cartItems.map(cart =>
-                    <li className='item' key={cart.id}>{cart.name}</li>
+                    <li className='item' key={cart.id}>
+                        <img className='profile' src={cart.image} alt="" />
+                        <h3>{cart.name}</h3>
+                    </li>
                 )
             }
             </ul>
